af864d9b8dd3e356a8f99019768fb891528daf17
GideonCove <gideoncove@gmail.com>
2024-07-24 16:52:22 -0400

    PatternsList funct

--- crochet_blaise/src/components/PatternsList.jsx
+++ crochet_blaise/src/components/PatternsList.jsx
@@ -1,9 +1,43 @@
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 
 export default function PatternsList () {
+    const [patterns, setPatterns] = useState("")
+    let { patternsId } = useParams()
+    let navigate = useNavigate()
+
+    useEffect (() => {
+        const getPatterns = async () => {
+            try{
+                const response = await axios.get(`http://localhost:800/patterns/`)
+                setPatterns(response)
+            } catch (error) {
+                console.error(`Cannot load events`, error)
+            }
+        }
+        getPatterns()
+    },[])
+
+    const showPattern = (index) => {
+        navigate(`${index}`)
+    }
     return (
         <div className = "patternsList">
-            
+            {
+                patterns != "" ? (                            
+                    patterns.data.map((pattern, index) => (                                
+                        <h1 className="map" key ={index} onClick={()=>showPattern(pattern.id)} >
+                        {console.log('pattern',pattern)}
+                            <ul>
+                                 {pattern.artist}
+                            </ul>
+                        </h1>
+                    ))
+                ) : (
+                    <h1>patterns have yet to be loaded</h1>
+                )
+            }
         </div>
     )
 }
