8f84ff2fa1bc0fe5bf8f169ebc6d191d7c043294
GideonCove <gideoncove@gmail.com>
2024-07-31 01:45:34 -0400

    responseive text sizing, media query for 600px and below

--- crochet_blaise/src/components/Footer.jsx
+++ crochet_blaise/src/components/Footer.jsx
@@ -3,7 +3,13 @@
 export default function Footer () {
     return (
         <div className ='footer'>
+            <div className ="Footer">
             
+            <a className="gideon footerlink" href ="https://github.com/gideonMCove" target="_blank">Gideon Cove </a>
+            <a className="git footerlink" href ="https://github.com/gideonMCove/Crochet-Blaise" target="_blank">Github Page</a>
+
+        </div>
+    
         </div>
     )
 }
